50c6f8a8dbdecb65434c6dd12b61d3686d547ad5
Dmitrii Pyzhov <d.pyzhov@andersenlab.com>
2022-09-12 10:22:53 +0400

    feat: Add empty project

---
+++ MenuBarWorldClock/MenuBarWorldClock/MenuBarWorldClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarWorldClockApp.swift
+//  MenuBarWorldClock
+//
+//  Created by Dmitry Pyzhov on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarWorldClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
